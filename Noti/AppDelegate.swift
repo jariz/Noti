47baea08436f628edab1e37c567f30b55872e7d2
Jari Zwarts <jarizw@gmail.com>
2016-06-22 17:01:14 +0200

    Initial Commit

---
+++ Noti/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Noti
+//
+//  Created by Jari on 22/06/16.
+//  Copyright © 2016 Oberon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
