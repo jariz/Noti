d15949257df17b4556c54168ef7018b0c8c94a1a
Jari Zwarts <jarizw@gmail.com>
2016-07-12 18:50:57 +0200

    Lock version numbers & fix compat with CSwift 0.5 (shoutout travis)

--- Noti/Crypt.swift
+++ Noti/Crypt.swift
@@ -3,7 +3,7 @@
 //  Noti
 //
 //  Created by Jari on 12/07/16.
-//  Copyright © 2016 Oberon. All rights reserved.
+//  Copyright © 2016 Jari Zwarts. All rights reserved.
 //
 
 import Foundation
@@ -20,7 +20,7 @@ public class Crypt {
         let password: Array<UInt8> = password.utf8.map {$0}
         let salt: Array<UInt8> = salt.utf8.map {$0}
         print("Noti is generating a key!")
-        return try? PKCS5.PBKDF2(password: password, salt: salt, iterations: 30000, keyLength: 32, hashVariant: .sha256).calculate()
+        return try? PKCS5.PBKDF2(password: password, salt: salt, iterations: 30000, keyLength: 32, variant: .sha256).calculate()
     }
     
     func decryptMessage(cipher: String) -> String? {
@@ -28,7 +28,7 @@ public class Crypt {
         var rawBytes = [UInt8](count: rawData!.length / sizeof(UInt8), repeatedValue: 0)
         rawData?.getBytes(&rawBytes, length: rawBytes.count)
         
-        let tag = [UInt8](rawBytes[1...16])
+//        let tag = [UInt8](rawBytes[1...16])
         let iv = [UInt8](rawBytes[17...28])
         let message = [UInt8](rawBytes[29..<rawBytes.count])
         
--- Podfile
+++ Podfile
@@ -2,8 +2,8 @@ use_frameworks!
 
 target 'Noti' do
     pod 'Starscream', '~> 1.1.3'
-    pod 'SwiftyJSON'
-    pod 'Alamofire', '~> 3.4'
-    pod 'Sparkle'
-    pod 'CryptoSwift'
+    pod 'SwiftyJSON', '~> 2.3.2'
+    pod 'Alamofire', '~> 3.4.1'
+    pod 'Sparkle', '~> 1.14.0'
+    pod 'CryptoSwift', '~> 0.5.1'
 end
