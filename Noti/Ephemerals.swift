0fb8a33bdc049011d8c195704767185376f61a13
Jakub Czekański <ja.czekanski@gmail.com>
2019-09-29 14:08:18 +0200

    Make notification_tag an optional parameter

--- Noti/Ephemerals.swift
+++ Noti/Ephemerals.swift
@@ -144,7 +144,7 @@ class Ephemerals: NSObject {
             "type": "push",
             "push": [
                 "notification_id": push["notification_id"].string!,
-                "notification_tag": push["notification_tag"].string!,
+                "notification_tag": push["notification_tag"].string,
                 "package_name": push["package_name"].string!,
                 "source_user_iden": push["source_user_iden"].string!,
                 "type": "dismissal"
